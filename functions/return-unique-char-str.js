35594a2d46a5d441e0ba157ab81a7a20b5ea2aa6
Bret Baker <bretbaker808@gmail.com>
2019-12-21 08:59:40 -0800

    function for returning strings with only unique characters added

--- functions/return-unique-char-str.js
+++ functions/return-unique-char-str.js
@@ -1,6 +1,6 @@
-function filterUnique(arr) {
+const filterUnique = arr => {
   console.log(arr.filter(x => new Set(x).size == x.length));
   return arr.filter(x => new Set(x).size == x.length);
-}
+};
 
 filterUnique(['abb', 'abc', 'abcdb', 'aea', 'bbb']);
