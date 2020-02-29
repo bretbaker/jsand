ed0e8fbbd84291fbd55a14c005a31bf60cf1a95c
Bret Baker <bretbaker808@gmail.com>
2020-02-29 10:00:35 -0800

    section 3 done

--- big-o-notation/spaceComplexity/linear.js
+++ big-o-notation/spaceComplexity/linear.js
@@ -1,7 +1,7 @@
 // O(n) || Linear
 
-// This function loops over an array, multiples the value at current index by 2
-// and hen pushe the new value into a new array and returns that new array
+// This function loops over an array, multiplies the value at current index by 2
+// and then pushes the new value into a new array and returns that new array
 // This function is considered linear because the space it requires grows
 // with the size of the input array
 
