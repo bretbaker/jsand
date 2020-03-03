97886da5f10c19fcad0041eda06614df9455efc8
Bret Baker <bretbaker808@gmail.com>
2020-03-03 11:59:30 -0800

    working on frequency counter

---
+++ frequencyCounter/bruteForce.js
@@ -0,0 +1,18 @@
+// O(n^2)
+// this function takes two arrays of integers and compares them, checking to see if the second
+// array contains an exact randomly sorted duplicate of the first array except each of the
+// values are squared
+// returns true if condition passes; false otherwise
+const same = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let ind = arr2.indexOf(arr1[i] ** 2);
+    if (ind === -1) {
+      return false;
+    }
+    arr2.splice(ind, 1);
+  }
+  return true;
+};
---
+++ frequencyCounter/finesse.js
@@ -0,0 +1,8 @@
+// O(n)
+// this function takes two arrays of integers and compares them, checking to see if the second
+// array contains an exact randomly sorted duplicate of the first array except each of the
+// values are squared
+// returns true if condition passes; false otherwise
+const same = (arr1, arr2) => {};
+
+same([1, 2, 3, 4, 5], [25, 16, 9, 4, 1]);
