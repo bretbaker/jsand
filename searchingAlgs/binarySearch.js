f9dafd27717d4ec29576407127133e7e769d4b42
Bret Baker <bretbaker808@gmail.com>
2020-03-14 10:04:31 -0700

    section 10 done

---
+++ searchingAlgs/binarySearch.js
@@ -0,0 +1,15 @@
+// O(log n)
+const binarySearch = (arr, elem) => {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+};
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
+binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8);
